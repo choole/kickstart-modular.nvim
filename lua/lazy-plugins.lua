654e33292ea99af3e4bf6c5be666014bd8009278
Nemanya <chukich1@outlook.com>
2024-06-07 18:04:46 +0200

    js lint

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -47,6 +47,7 @@ require('lazy').setup({
   require 'kickstart/plugins/mini',
 
   require 'kickstart/plugins/treesitter',
+  -- require 'kickstart/plugins/lint',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -59,7 +60,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.lint',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
